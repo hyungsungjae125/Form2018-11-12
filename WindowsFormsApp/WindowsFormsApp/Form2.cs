7fd00b655729b0e278fad65123027d067116942a
형성재 <tldwo125@naver.com>
2018-11-12 16:10:23 +0900

    ...

--- WindowsFormsApp/WindowsFormsApp/Form2.cs
+++ WindowsFormsApp/WindowsFormsApp/Form2.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp
         {
             InitializeComponent();
         }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
